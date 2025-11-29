c914d766f9a7dfbf9953b28d367c2d0a8cb37e96
Gia Thành <71538542+xing215@users.noreply.github.com>
2025-11-29 20:08:44 +0700

    feat: mockup hardware architecture (#1)

---
+++ hardware/NomNom/NomNom.ino
@@ -0,0 +1,12 @@
+#include "libs.h"
+#include "LimitSwitch.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  LimitSwitch_setup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  LimitSwitch_loop();
+}
