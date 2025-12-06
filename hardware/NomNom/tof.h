53a25c30bd3c4065c7d3c594cb8cfef6a741778a
Gia Thành <71538542+xing215@users.noreply.github.com>
2025-12-06 19:34:23 +0700

    fix: improve TOF sensor initialization and error handling

--- hardware/NomNom/tof.h
+++ hardware/NomNom/tof.h
@@ -8,13 +8,14 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 #define EMPTY_DISTANCE 5000 // 5000 mm = 5m
 
+static bool TOF_SUCCESS = false;
+
 int tof_getDistance() {
   VL53L0X_RangingMeasurementData_t measure;
     
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
     return measure.RangeMilliMeter;
   } else {
     return -1;
@@ -24,14 +25,19 @@ int tof_getDistance() {
 void tof_setup() {
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
+    TOF_SUCCESS = false;
   }
+  else
+    TOF_SUCCESS = true;
 }
 
 void tof_loop() {
   int distance = tof_getDistance();
-  if (distance >= EMPTY_DISTANCE) {
+  if (!TOF_SUCCESS || distance == -1)
+    Serial.println(F("Failed to boot VL53L0X"));
+  else if (distance >= EMPTY_DISTANCE) {
     Serial.print("Sending empty storage warning with distance = ");
-    Serial.print(distance);
+    Serial.println(distance);
     String formatedData = "{\"distance\":";
     formatedData += distance;
     formatedData += "}";
