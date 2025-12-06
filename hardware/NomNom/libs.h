62f27eb5eaac2a1967ff4c3f9f733e969be35ebe
heyyouknowme <ntntran23@clc.fitus.edu.vn>
2025-12-06 20:19:27 +0700

    loadcell

--- hardware/NomNom/libs.h
+++ hardware/NomNom/libs.h
@@ -1,7 +1,13 @@
+
 #ifndef LIBS_H
 #define LIBS_H
 
-#include "Adafruit_VL53L0X.h"
+// HERE
 #include <DHT.h>
+#include <Adafruit_HX711.h>
+//#include <DHT_U.h>
+
+//#include <Adafruit_CircuitPlayground.h>
+//#include <Adafruit_Circuit_Playground.h>
 
 #endif
