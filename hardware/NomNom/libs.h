75c7bdbf9e94c232a1c7ff2a836597879d926b28
Gia Thành <71538542+xing215@users.noreply.github.com>
2025-12-06 20:22:09 +0700

    Clean up includes in libs.h
    Removed unused includes and commented lines.

--- hardware/NomNom/libs.h
+++ hardware/NomNom/libs.h
@@ -2,12 +2,7 @@
 #ifndef LIBS_H
 #define LIBS_H
 
-// HERE
 #include <DHT.h>
 #include <Adafruit_HX711.h>
-//#include <DHT_U.h>
-
-//#include <Adafruit_CircuitPlayground.h>
-//#include <Adafruit_Circuit_Playground.h>
 
 #endif
